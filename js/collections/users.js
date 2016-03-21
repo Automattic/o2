541065e547c941600755a36e22bc9574da3e6028
Jeffrey Walter <jeffreydwalter@gmail.com>
2016-03-21 10:05:46 -0700

    Fix https://github.com/Automattic/o2/issues/16
    Changed the css selector to 'o2-incomplete-' + user.hash instead of 'o2-incomplete-' + user.userLogin, to avoid having to escape the selector.

--- js/collections/users.js
+++ js/collections/users.js
@@ -37,7 +37,7 @@ o2.Collections.Users = ( function( $, Backbone ) {
 					user = new o2.Models.User( {
 						userLogin   : object.userLogin,
 						displayName : object.userLogin,
-						modelClass  : 'o2-incomplete-' + object.userLogin
+						modelClass  : 'o2-incomplete-' + object.hash
 					} );
 
 					// add it to the collection
@@ -115,8 +115,7 @@ o2.Collections.Users = ( function( $, Backbone ) {
 
 			var user = this.getUserFor( { userLogin: userLogin } );
 
-			// update img src's and a href's with .o2-incomplete-{userLogin}
-			var selectorClass = 'o2-incomplete-' + userLogin;
+			var selectorClass = 'o2-incomplete-' + user.hash; 
 
 			$( 'a.' + selectorClass ).each( function() {
 				$( this ).attr( 'href', user.url ).removeClass( selectorClass );
@@ -138,4 +137,3 @@ o2.Collections.Users = ( function( $, Backbone ) {
 
 	} );
 } )( jQuery, Backbone );
-
