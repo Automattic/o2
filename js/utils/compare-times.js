fa607e21adc1d26093eb4cdeb50b3b66d8f203d4
George Stephanis <georgestephanis@automattic.com>
2016-03-25 10:14:25 -0400

    JSHint: four utils

--- js/utils/compare-times.js
+++ js/utils/compare-times.js
@@ -3,7 +3,8 @@ var o2 = o2 || {};
 o2.Utilities = o2.Utilities || {};
 
 o2.Utilities.compareTimes = function( t1, t2 ) {
-	if ( t1 == t2 )
+	if ( t1 === t2 ) {
 		return 0;
+	}
 	return t1 < t2 ? -1 : 1;
 };
