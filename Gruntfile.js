afcba0d291b45dc77ecacbff4481c716355cf319
George Stephanis <georgestephanis@automattic.com>
2016-03-24 20:15:07 -0400

    Convert indents, and minor tidying with specifying of files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,56 @@
 /* global module */
 module.exports = function(grunt) {
-    var cfg = {
-            pkg: grunt.file.readJSON('package.json'),
-            phplint: {
-                files: [
-                    '*.php',
+	var cfg = {
+			pkg: grunt.file.readJSON('package.json'),
+			phplint: {
+				files: [
+					'*.php',
 					'**/*.php'
-                ]
-            },
-            jshint: {
-                options: grunt.file.readJSON('.jshintrc'),
-                src: [
-                    'js/**/*.js',
+				]
+			},
+			jshint: {
+				options: grunt.file.readJSON('.jshintrc'),
+				src: [
+					'js/**/*.js',
 					'modules/**/*.js'
-                ]
-            },
-            makepot: {
-                o2: {
-                    options: {
-                        domainPath: '/languages',
-                        exclude: [
-                            'node_modules'
-                        ],
-                        mainFile:    'o2.php',
-                        potFilename: 'o2.pot'
-                    }
-                }
-            },
-            addtextdomain: {
-                o2: {
-                    options: {
-                        textdomain: 'o2'
-                    },
-                    files: {
-                        src: [
-                            '**.php',
-                            '!node_modules/**'
-                        ]
-                    }
-                }
-            }
-        };
+				]
+			},
+			makepot: {
+				o2: {
+					options: {
+						domainPath: '/languages',
+						exclude: [
+							'node_modules'
+						],
+						mainFile:    'o2.php',
+						potFilename: 'o2.pot'
+					}
+				}
+			},
+			addtextdomain: {
+				o2: {
+					options: {
+						textdomain: 'o2'
+					},
+					files: {
+						src: [
+							'*.php',
+							'**/*.php',
+							'!node_modules/**'
+						]
+					}
+				}
+			}
+		};
 
-    grunt.initConfig( cfg );
+	grunt.initConfig( cfg );
 
-    grunt.loadNpmTasks('grunt-phplint');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-wp-i18n');
+	grunt.loadNpmTasks('grunt-phplint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-wp-i18n');
 
-    grunt.registerTask('default', [
-        'phplint',
-        'jshint'
-    ]);
+	grunt.registerTask('default', [
+		'phplint',
+		'jshint'
+	]);
 };
