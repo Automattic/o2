5488e5f18d72bab50f211c3b9d52f9c4257f5104
George Stephanis <georgestephanis@automattic.com>
2016-03-24 21:39:01 -0400

    JSHint post-actions

--- modules/post-actions/js/script.js
+++ modules/post-actions/js/script.js
@@ -2,10 +2,10 @@ var o2 = o2 || {};
 
 o2.Utilities = o2.Utilities || {};
 
-o2.Utilities.closeActionDropdowns = function( event ) {
+o2.Utilities.closeActionDropdowns = function() {
 	// Raise an event on the o2 container to prompt post views to close open disclosures
 	o2.Events.doAction( 'dropdown-actions:closeall.o2', {} );
-}
+};
 
 jQuery( document ).click( function( event ) {
 	o2.Utilities.closeActionDropdowns( event );
