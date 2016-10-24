Automattic/o2

commit 8519ae8e6a40e7559b509446f239b1edaf63f7e4

Editor: Allow post titles with quotes in them to be edited.
Fixes #21.

js/editor/editor.js
@@ -143,7 +143,7 @@ o2Editor = {
 		// Prime details from the <textarea> we're replacing
 		var $holder = $( elem ),
 				prompt  = $holder.attr( 'placeholder' ) || '',
-				title   = $holder.attr( 'title' ), // Leave attribute off <textarea> completely to remove title controls
+				title   = $holder.attr( 'title' ).replace( /"/g, '&quot;' ), // Leave attribute off <textarea> completely to remove title controls
 				content = $holder.val(),
 				editor;
 

commit a90798f5c3b36cdbcea3015d27ed1bd21d1a85ec

Editor: Check that the title exists before trying to escape it.
Fixes a bug introduced in 8519ae8.

See #21.

js/editor/editor.js
@@ -143,9 +143,13 @@ o2Editor = {
 		// Prime details from the <textarea> we're replacing
 		var $holder = $( elem ),
 				prompt  = $holder.attr( 'placeholder' ) || '',
-				title   = $holder.attr( 'title' ).replace( /"/g, '&quot;' ), // Leave attribute off <textarea> completely to remove title controls
+				title   = $holder.attr( 'title' ), // Leave attribute off <textarea> completely to remove title controls
 				content = $holder.val(),
 				editor;
+
+				if ( 'string' === typeof title ) {
+					title = title.replace( /"/g, '&quot;' );
+				}
 
 		editor = '<div class="o2-editor-wrapper"> \
 			<div class="o2-editor-toolbar-wrapper"> \