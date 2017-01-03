91ec2687f963ca55320ed127f0a4d894ab1f4edf
Gary Pendergast <gary@pento.net>
2017-01-03 16:04:21 +1100

    Fix some jshint warnings in the time shortcake.

--- modules/time-shortcode/js/time-shortcode.js
+++ modules/time-shortcode/js/time-shortcode.js
@@ -1,10 +1,13 @@
+/* exported o2_format_date */
+/* globals o2_get_time_settings */
+
 /**
  * Get a nice, localized textual representation of a date
  *
  * @param date - date object
  * @return string
  **/
-var o2_format_date = function ( date ) {
+var o2_format_date = function( date ) {
 
 	var time_settings = o2_get_time_settings();
 
@@ -12,15 +15,15 @@ var o2_format_date = function ( date ) {
 		return ( '00' + n ).slice( -2 );
 	};
 
-	timezone_offset = -date.getTimezoneOffset() / 60;
+var timezone_offset = -date.getTimezoneOffset() / 60;
 	if( timezone_offset >= 0 ) {
-		timezone_offset = "+" + timezone_offset;
+		timezone_offset = '+' + timezone_offset;
 	}
 
-	return  time_settings['days'][ date.getDay() ] + ", "
-		+ time_settings['months'][ date.getMonth() ] + " "
-		+ zero_prefix( date.getDate() ) + ", "
-		+ date.getFullYear() + " "
-		+ zero_prefix( date.getHours() ) + ":" + zero_prefix( date.getMinutes() )
-		+ " UTC" + timezone_offset;
-}
+	return time_settings.days[ date.getDay() ] + ', ' +
+		time_settings.months[ date.getMonth() ] + ' ' +
+		zero_prefix( date.getDate() ) + ', ' +
+		date.getFullYear() + ' ' +
+		zero_prefix( date.getHours() ) + ':' + zero_prefix( date.getMinutes() ) +
+		' UTC' + timezone_offset;
+};
