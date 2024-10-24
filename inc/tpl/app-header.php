a5bf254164240a9c7f6a6312af22b6b4706e0e50
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-10-24 15:28:22 +1300

    Use h1 for all page titles
    Fixes heading hierarchy issues on archive pages

--- inc/tpl/app-header.php
+++ inc/tpl/app-header.php
@@ -1,9 +1,9 @@
 <# if ( !data.isSingle && !data.isPage && !data.is404 && !( data.isSearch && !data.havePosts ) ) { #>
-	<h2 class="o2-app-page-title">
+	<h1 class="o2-app-page-title">
 		<# if ( data.showTitle && data.pageTitle != '' ) { #>
 			{{{ data.pageTitle }}}
 		<# } #>
-	</h2>
+	</h1>
 	<span class="o2-app-controls">
 		{{{ data.appControls.join( ' | ' ) }}}
 	</span>
